c8fc80596b5c3d42bbd550bfc956bf5da355cd3f
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2018-12-01 09:33:53 -0500

    fuck this, fuck everything, why does it have to be hard

---
+++ day1/part2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#god i fucking love when i dont know what to do
+#i always feel like im getting there and then it doesnt work xd
+""" with open("input.txt") as file:
+    lines = file.readlines()
+    freq2 = []
+    for line in lines:
+        freq2 += str(freq2.append(line))
+        #for i in list.index(freq2):
+        if freq2[:] == freq2:
+            print("The repeated frequency is: " + str(freq2))
+        #freq2 += int(line)
+    print(freq2) """
